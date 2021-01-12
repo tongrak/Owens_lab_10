b13c453e4f429eb538c6acbb643b72996a77baa7
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2021-01-12 15:15:11 +0700

    Lab10_1 Done!!

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+int main(){
+    int i;
+    string grade[9]={"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    srand(time(0));
+    cin.get();
+    i = rand()%9;
+    cin.get();
+    cout << "You will get " <<grade[i]<< " in this 261102.";
+
+    return 0;
+}
